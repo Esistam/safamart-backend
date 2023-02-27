468a74eb9dce89333263c3aefc3463a4d3d63617
Kadweka <husseinkatana17@gmail.com>
2023-02-27 20:41:40 +0300

    update api module

--- custom-addons/eccomerce-api/controller/main.py
+++ custom-addons/eccomerce-api/controller/main.py
@@ -10,105 +10,157 @@ import logging
 
 _logger = logging.getLogger(__name__)
 today = datetime.today()
+
+
 class MainController(http.Controller):
-    @http.route('/view/catgory/products', type='json', auth="public", cors='*', method=['POST'])
-    def view__category_propducts(self,**kw):
+    @http.route('/view/category/products', type='json', auth="public", cors='*', method=['POST'])
+    def view__category_propducts(self, **kw):
         payload = json.loads(request.httprequest.data)
-        data=[]
-        category = request.env['product.category'].sudo().search([('is_advert', "=", True)],limit=2)
-        product = request.env['product.template'].sudo().search([('is_advert', "=", True)],limit=16)
-        [data.append({"nameView":x.name,"descView":x.description,"titleView":x.title,"imageView":x.attachment,"lines":[{
-        "productName":y.name,
-        "productId":y.id,
-        "productPrice":y.list_price,
-        "productCategory":y.categ_id.name,
-        "productImage":y.image_1920,
-        "productDescription":y.description
-        } for y in product if y.categ_id.name == x.name]})for x in category]
-        response={
-            "code":200,
-            "status":"Successfull",
-            "items":data
-        }
+        data = []
+                # domain=["|",("categ_id", "=",payload["category_id"]),("name", "ilike",payload['name'])]
+        product = request.env['product.template'].sudo().search(["&",("categ_id.id",'=',payload['category_id']),("name",'ilike',payload['name'])], limit=8)
+        [data.append({"productName": x.name,"productId": x.id,"productPrice": x.list_price,"productCategory": x.categ_id.name,"productImage": x.image_1920,"productDescription": x.description})for x in product]
+        response = data
+        return response
+    @http.route('/view/category/information', type='json', auth="public", cors='*', method=['POST'])
+    def view__category_information(self, **kw):
+        payload = json.loads(request.httprequest.data)
+        data = []
+        category = request.env['product.category'].sudo().search([('is_advert', "=", True),("id","=",payload['category_id'])])
+        [data.append({"categoryName":x.name,"categoryId":x.id,"categoryImage":x.attachment,"categoryDescription":x.description})for x in category]
+        response = data
         return response
     @http.route('/view/raw/products', type='json', auth="public", cors='*', method=['POST'])
-    def view__raw_products(self):
-        data=[]
-        product = request.env['product.template'].sudo().search([('sale_ok', "=", True),('is_advert',"=",True)],limit=20)
-        for res in product:
-            vals={
-                "productName":res.name,
-                "productId":res.id,
-                "productPrice":res.list_price,
-                "productCategory":res.categ_id.name,
-                "productImage":res.image_1920,
-                "productDescription":res.description
-            }
-            data.append(vals)
-        response={
-            "code":200,
-            "status":"Successfull",
-            "items":data
+    def view__raw_products(self,**kw):
+        payload = json.loads(request.httprequest.data)
+        data1 = []
+        data2 = []
+        product = request.env['product.template'].sudo().search(["&",("is_advert","=",False),('sale_ok', "=", True),("name","ilike",payload["name"])],limit=24)
+        product2 = request.env['product.template'].sudo().search(["&",("is_advert","=",True),('sale_ok', "=", True),("name","ilike",payload["name"])],limit=24)
+        [data1.append({"productName": x.name,"productId": x.id,"productPrice": x.list_price,"productCategory": x.categ_id.name,"productImage": x.image_1920,"productDescription": x.description})for x in product]
+        [data2.append({"productName": x.name,"productId": x.id,"productPrice": x.list_price,"productCategory": x.categ_id.name,"productImage": x.image_1920,"productDescription": x.description})for x in product2]
+        response = {
+            "code": 200,
+            "status": "Successfully",
+            "item1": data1,
+            "item2": data2
         }
         return response
-    @http.route('/view/products', type='json', auth="public", cors='*', method=['POST'])
-    def view__products(self):
-        query = """select id,name,list_price,categ_id from product_template LIMIT 20"""
-        request.cr.execute(query)    
-        products = request.cr.dictfetchall() 
-        data={
-            "code":200,
-            "status":"success",
-            "result":products
+
+
+    @http.route("/search_read",type='json', auth="public", cors='*', method=['POST'])
+    def search_products(self,**kw):
+        payload = json.loads(request.httprequest.data)
+        data = []
+        domain=["|",("sale_ok", "=",True),("name", "ilike",payload['name'])]
+        product = request.env['product.template'].sudo().search(domain, limit=20)
+        [data.append({"productName": x.name,"productId": x.id,"productPrice": x.list_price,"productCategory": x.categ_id.name,"productImage": x.image_1920,"productDescription": x.description})for x in product]
+        response = data
+        return response
+
+    @http.route('/view/category', type='json', auth="public", cors='*', method=['POST'])
+    def view__raw_category(self,**kw):
+        payload = json.loads(request.httprequest.data)
+        data = []
+        category = request.env['product.category'].sudo().search(["&",('is_advert', "=", True),("name","ilike",payload["name"])])
+        [data.append({"imgView":x.attachment,"categView":x.id,"categName":x.name}) for x in category]
+        response = {
+            "code": 200,
+            "status": "Successfully",
+            "items": data
         }
+        return response
+
+
+
+
+
+
+
+
+
+
+
+# SINGLE PAGE SECTION ENDPOINTS
+    @http.route("/related_products",type="json", auth="public", cors="*", method=["POST"])
+    def view_related_product_reviews(self,**kw):
+        payload = json.loads(request.httprequest.data)
+        data = []
+        products = request.env['product.template'].sudo().search(["&",("categ_id.id", "=", payload['categ_id']),('name',"ilike",payload['name'])],limit=28)
+        [data.append({
+            "productName": x.name,
+                "productId": x.id,
+                "productPrice": x.list_price,
+                "productCategory": x.categ_id,
+                "productRating": x.rating,
+                "productImage": x.image_1920,
+                "categoryName": x.categ_id.name,
+                "productDescription": x.description}) for x in products]
         return data
-    @http.route('/view/categories', type='json', auth="public", cors='*', method=['POST'])
-    def view__categories(self):
-        query = """select id,name from product_category where is_active=True"""
-        request.cr.execute(query)    
-        category = request.cr.dictfetchall() 
-        data={
-            "code":200,
-            "status":"success",
-            "result":category
-        }
+    @http.route("/my/review",type="json", auth="public", cors="*", method=["POST"])
+    def view_single_product_reviews(self,**kw):
+        payload = json.loads(request.httprequest.data)
+        data = []
+        reviews = request.env['product.reviews'].sudo().search([("product_id.id", "=", payload['product_id'])])
+        [data.append({"reviewAuthor": y.name, "reviewMessage": y.message, "reviewRate": int(y.rate)}) for y in reviews]
         return data
-    @http.route('/view/prod_details', type='json', auth="public", cors='*', method=['POST'])
-    def view__product_details(self,**kws):
+    @http.route('/new/review', type="json", auth="public", cors="*", method=["POST"])
+    def new_review(self, **kw):
         data = json.loads(request.httprequest.data)
-        product_id = data['product_id']
-        query = """select id,name,list_price,categ_id from product_template where id=%s""" %product_id
-        query2 = """select id,name,message,rate,product_id from product_reviews where product_id=%s""" %product_id
-        request.cr.execute(query) 
-        category = request.cr.dictfetchall()
-        request.cr.execute(query2)    
-        ratings = request.cr.dictfetchall() 
-        data={
-            "code":200,
-            "status":"success",
-            "result":category,
-            "ratings":ratings
-        }
-        return data
+        author = data['name']
+        star = data['rate']
+        message = data['message']
+        productId = data['product_id']
+        if not productId:
+            response = {
+                'code': 400,
+                'message': 'Product Id cannot be empty cannot be empty'
+            }
+            return response
+        if not author:
+            response = {
+                'code': 400,
+                'message': 'Name cannot be empty cannot be empty'
+            }
+            return response
+        if not star:
+            response = {
+                'code': 400,
+                'message': 'Rating cannot be empty'
+            }
+            return response
+        if not message:
+            response = {
+                'code': 400,
+                'message': 'Message cannot be empty'
+            }
+            return response
+        rating = request.env['product.reviews'].sudo().create({
+            'name': author.lower(),
+            'message': message.lower(),
+            'rate': star,
+            'product_id': productId
+        })
+        if rating:
+            return {
+                "code": 200,
+                "status": "Success",
+                "message": "You Have created a review"
+            }
 
     @http.route('/view/details/products', type='json', auth="public", cors='*', method=['POST'])
-    def view__details_products(self,**kw):
+    def view__details_products(self, **kw):
         payload = json.loads(request.httprequest.data)
-        data=[]
-        product = request.env['product.template'].sudo().search([("id","=",payload['product_id'])])
-        for res in product:
-            vals={
-                "productName":res.name,
-                "productId":res.id,
-                "productCategory":res.categ_id.name,
-                "productPrice":res.list_price,
-                "productImage":res.image_1920,
-                "productDescription":res.description
-            }
-            data.append(vals)
-        response={
-            "code":200,
-            "status":"Successfull",
-            "items":data
-        }
-        return response
+        data = []
+        product = request.env['product.template'].sudo().search([("id", "=", payload['product_id'])])
+        [data.append(
+            {
+                "productName": x.name,
+                "productId": x.id,
+                "productRating": x.rating,
+                "productPrice": x.list_price,
+                "productCategory": x.categ_id.id,
+                "productImage": x.image_1920,
+                "categoryName": x.categ_id.name,
+                "productDescription": x.description}) for x in product]
+        return data
--- custom-addons/eccomerce-api/models/Reviews.py
+++ custom-addons/eccomerce-api/models/Reviews.py
@@ -11,25 +11,38 @@ class ProductReview(models.Model):
     product_id=fields.Many2one("product.template",string="Product",readonly=True)
     rate=fields.Selection([
             ("1",1),
-            ("1",2),
-            ("1",3),
-            ("1",4),
-            ("1",5)
+            ("2",2),
+            ("3",3),
+            ("4",4),
+            ("5",5)
     ],string="Ratings",readonly=True)
 
 class ProductCatgory(models.Model):
         _inherit = 'product.category'
 
+      
+                
         attachment = fields.Binary(string="Attachment")    
         store_fname = fields.Char(string="File Name")
         description=fields.Char("Description")
         title=fields.Char("Title")
         is_offer=fields.Boolean(string="Has Offer")
         offer_value=fields.Float(string="rate %")
-        is_advert=fields.Boolean(string="Is Advert")
+        is_advert=fields.Boolean(string="Is Advert",default=True)
 
 
 class ProductTemplate(models.Model):
         _inherit = 'product.template'
 
+        @api.depends('rate_ids')
+        def _compute_product_ratings(self):
+                totalRating=0
+                items=len(self.rate_ids)
+                for rec in self:
+                    for res in rec.rate_ids:
+                        totalRating+=int(res.rate)
+                value=totalRating/items if len(self.rate_ids)>0 else 0
+                self.rating=value
         is_advert=fields.Boolean(string="Is Advert")
+        rate_ids=fields.One2many("product.reviews",'product_id',string="Product")
+        rating=fields.Float(string="Ratings",compute='_compute_product_ratings')
